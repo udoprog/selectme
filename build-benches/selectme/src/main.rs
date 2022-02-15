8c46c843e850bbfddcd3c83b97f7112535996e7b
John-John Tedro <udoprog@tedro.se>
2022-02-15 15:59:13 +0100

    Add build-benches

---
+++ build-benches/selectme/src/main.rs
@@ -0,0 +1,91 @@
+use std::future::ready;
+
+pub fn main() {
+    let _ = test();
+}
+
+async fn test() {
+    let (v0, v1, v2, v3) = (ready(()), ready(()), ready(()), ready(()));
+    let (v4, v5, v6, v7) = (ready(()), ready(()), ready(()), ready(()));
+    let (v8, v9, v10, v11) = (ready(()), ready(()), ready(()), ready(()));
+    let (v12, v13, v14, v15) = (ready(()), ready(()), ready(()), ready(()));
+    let (v16, v17, v18, v19) = (ready(()), ready(()), ready(()), ready(()));
+    let (v20, v21, v22, v23) = (ready(()), ready(()), ready(()), ready(()));
+    let (v24, v25, v26, v27) = (ready(()), ready(()), ready(()), ready(()));
+    let (v28, v29, v30, v31) = (ready(()), ready(()), ready(()), ready(()));
+    let (v32, v33, v34, v35) = (ready(()), ready(()), ready(()), ready(()));
+    let (v36, v37, v38, v39) = (ready(()), ready(()), ready(()), ready(()));
+    let (v40, v41, v42, v43) = (ready(()), ready(()), ready(()), ready(()));
+    let (v44, v45, v46, v47) = (ready(()), ready(()), ready(()), ready(()));
+    let (v48, v49, v50, v51) = (ready(()), ready(()), ready(()), ready(()));
+    let (v52, v53, v54, v55) = (ready(()), ready(()), ready(()), ready(()));
+    let (v56, v57, v58, v59) = (ready(()), ready(()), ready(()), ready(()));
+    let (v60, v61, v62, v63) = (ready(()), ready(()), ready(()), ready(()));
+
+    selectme::select! {
+        _ = v0 => {}
+        _ = v1 => {}
+        _ = v2 => {}
+        _ = v3 => {}
+        _ = v4 => {}
+        _ = v5 => {}
+        _ = v6 => {}
+        _ = v7 => {}
+        _ = v8 => {}
+        _ = v9 => {}
+        _ = v10 => {}
+        _ = v11 => {}
+        _ = v12 => {}
+        _ = v13 => {}
+        _ = v14 => {}
+        _ = v15 => {}
+        _ = v16 => {}
+        _ = v17 => {}
+        _ = v18 => {}
+        _ = v19 => {}
+        _ = v20 => {}
+        _ = v21 => {}
+        _ = v22 => {}
+        _ = v23 => {}
+        _ = v24 => {}
+        _ = v25 => {}
+        _ = v26 => {}
+        _ = v27 => {}
+        _ = v28 => {}
+        _ = v29 => {}
+        _ = v30 => {}
+        _ = v31 => {}
+        _ = v32 => {}
+        _ = v33 => {}
+        _ = v34 => {}
+        _ = v35 => {}
+        _ = v36 => {}
+        _ = v37 => {}
+        _ = v38 => {}
+        _ = v39 => {}
+        _ = v40 => {}
+        _ = v41 => {}
+        _ = v42 => {}
+        _ = v43 => {}
+        _ = v44 => {}
+        _ = v45 => {}
+        _ = v46 => {}
+        _ = v47 => {}
+        _ = v48 => {}
+        _ = v49 => {}
+        _ = v50 => {}
+        _ = v51 => {}
+        _ = v52 => {}
+        _ = v53 => {}
+        _ = v54 => {}
+        _ = v55 => {}
+        _ = v56 => {}
+        _ = v57 => {}
+        _ = v58 => {}
+        _ = v59 => {}
+        _ = v60 => {}
+        _ = v61 => {}
+        _ = v62 => {}
+        _ = v63 => {}
+    }
+}
